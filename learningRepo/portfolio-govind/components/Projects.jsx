136261d8d2b18b97d9709d92dcf7c0d51b87c0b5
Govind Patlolu <govindreddy521@gmail.com>
2022-09-26 09:52:34 -0500

    Build error fixing third one

--- learningRepo/portfolio-govind/components/Projects.jsx
+++ learningRepo/portfolio-govind/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
     <div id='projects' className='w-full'>
         <div className='max-w-[1240px] mx-auto px-2 py-16 '>
             <p className='text-xl tracking-widest uppercase text-[#2c69af]'>Projects</p>
-            <h2 className='py-4'>What I've Built</h2>
+            <h2 className='py-4'>What I\'ve Built</h2>
             <div className='grid md:grid-cols-2 gap-8'>
             <ListofProject title={'Blog Template'} imagePath={Blog} projectUrl={'/Blog'} />
             <ListofProject title={'Wireless Internet'} imagePath={ATT} projectUrl={'/TeleCom'} />
